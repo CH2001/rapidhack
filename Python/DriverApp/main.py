aca52e929ece4595513a1d43f81d780ad9ab65dd
Edmund_IdeaPad <elz.zhenming@gmail.com>
2023-09-14 15:27:49 +0800

    feat: create classes

--- Python/DriverApp/main.py
+++ Python/DriverApp/main.py
@@ -1,1 +1,18 @@
 import kivy
+
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.uix.boxlayout import BoxLayout
+
+class MyRoot(BoxLayout):
+    
+    def __init__(self):
+        super(MyRoot, self).__init__()
+
+class DriverApp(App):
+    
+    def build(self):
+        return MyRoot()
+    
+driverApp = DriverApp()
+driverApp.run()
