e9476d50cc0ad0c60ba7533c0e6111f3010ebd97
CHUN HANSSEN <65500133+CH2001@users.noreply.github.com>
2023-09-23 10:12:12 +0000

    logo

--- pages/components/GuideTitle.jsx
+++ pages/components/GuideTitle.jsx
@@ -6,33 +6,37 @@ import Link from "next/link";
 const GuideTitle = () => {
 	return (
 		<div>
-			<div className="flex items-center justify-center mt-5 mb-3"> 
-            	<Image src={"/images/PlanYourTrip.png"} width={550} height={100} alt="plan your trip"/>
+			<div className="flex items-center justify-center mt-5 mb-3 ml-3 mr-3"> 
+            	<Image src={"/images/PlanYourTrip.png"} width={450} height={250} alt="plan your trip"/>
         	</div>
 
 			<div style={{ display: 'flex'}}>
 				<div style={{ flex: '14%' }}>
-					<button className="rounded-2xl flex items-center bg-light-blue p-2 mx-auto" style={{ width: '50px', textAlign: 'center' }}>
-						<div style={{ fontSize: '14px' }} className="relative capitalize">KL Twin Tower</div>
+					<button className="rounded-full flex items-center bg-light-blue p-2 mx-auto" style={{ width: '50px', textAlign: 'center' }}>
+						<Image src={"/images/KLCC.png"} width={20} height={20}/>
 					</button>
+					<div style={{ fontSize: '14px' }} className="relative capitalize">KL Twin Tower</div>
 				</div>
 
 				<div style={{ flex: '14%' }}>
-					<button className="rounded-2xl flex items-center bg-light-blue p-2 mx-auto" style={{ width: '50px', textAlign: 'center' }}>
-						<div style={{ fontSize: '14px' }} className="relative capitalize">Zoo Negara</div>
+					<button className="rounded-full flex items-center bg-light-blue p-2 mx-auto" style={{ width: '50px', textAlign: 'center' }}>
+						<Image src={"/images/ZooNegara.png"} width={20} height={20}/>
 					</button>
+					<div style={{ fontSize: '14px' }} className="relative capitalize">Zoo Negara</div>
 				</div>
 
 				<div style={{ flex: '14%' }}>
-					<button className="rounded-2xl flex items-center bg-light-blue p-2 mx-auto" style={{ width: '50px', textAlign: 'center' }}>
-						<div style={{ fontSize: '14px' }} className="relative capitalize">Batu Caves</div>
+					<button className="rounded-full flex items-center bg-light-blue p-2 mx-auto" style={{ width: '50px', textAlign: 'center' }}>
+						<Image src={"/images/BatuCaves.png"} width={20} height={20}/>
 					</button>
+					<div style={{ fontSize: '14px' }} className="relative capitalize">Batu Caves</div>
 				</div>
 
 				<div style={{ flex: '14%' }}>
-					<button className="rounded-2xl flex items-center bg-light-blue p-2 mx-auto" style={{ width: '50px', textAlign: 'center' }}>
-						<div style={{ fontSize: '14px' }} className="relative capitalize">National Museum</div>
+					<button className="rounded-full flex items-center bg-light-blue p-2 mx-auto" style={{ width: '50px', textAlign: 'center' }}>
+						<Image src={"/images/NationalMuzeum.png"} width={20} height={20}/>
 					</button>
+					<div style={{ fontSize: '14px' }} className="relative capitalize">National Museum</div>
 				</div>
 			</div>
 		</div>
--- pages/guide.tsx
+++ pages/guide.tsx
@@ -11,7 +11,6 @@ const Guide = () => {
       <div className="[background:linear-gradient(180deg,_#4c2ffd_48.23%,_rgba(0,_224,_255,_0)_99.99%,_rgba(255,_67,_67,_0))]"> 
         <TopPageNavigation/> 
       </div>
-      This is guide.
       <GuideTitle/>
       <BottomPageNavigation/>
     </div>
