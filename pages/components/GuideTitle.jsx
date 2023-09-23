96537e85d22b091fa2a40eb5c63375b7ef9140c3
CHUN HANSSEN <65500133+CH2001@users.noreply.github.com>
2023-09-23 09:42:06 +0000

    button

--- pages/components/GuideTitle.jsx
+++ pages/components/GuideTitle.jsx
@@ -6,28 +6,34 @@ import Link from "next/link";
 const GuideTitle = () => {
 	return (
 		<div>
-			<div style={{ flex: '14%' }}>
-				<button className="rounded-2xl flex items-center bg-light-blue p-2 mx-auto" style={{ width: '50px', textAlign: 'center' }}>
-					<div style={{ fontSize: '14px' }} className="relative capitalize">KL Twin Tower</div>
-				</button>
-			</div>
+			<div className="flex items-center justify-center mt-5 mb-3"> 
+            	<Image src={"/images/PlanYourTrip.png"} width={550} height={100} alt="plan your trip"/>
+        	</div>
 
-			<div style={{ flex: '14%' }}>
-				<button className="rounded-2xl flex items-center bg-light-blue p-2 mx-auto" style={{ width: '50px', textAlign: 'center' }}>
-					<div style={{ fontSize: '14px' }} className="relative capitalize">Zoo Negara</div>
-				</button>
-			</div>
+			<div style={{ display: 'flex'}}>
+				<div style={{ flex: '14%' }}>
+					<button className="rounded-2xl flex items-center bg-light-blue p-2 mx-auto" style={{ width: '50px', textAlign: 'center' }}>
+						<div style={{ fontSize: '14px' }} className="relative capitalize">KL Twin Tower</div>
+					</button>
+				</div>
 
-			<div style={{ flex: '14%' }}>
-				<button className="rounded-2xl flex items-center bg-light-blue p-2 mx-auto" style={{ width: '50px', textAlign: 'center' }}>
-					<div style={{ fontSize: '14px' }} className="relative capitalize">Batu Caves</div>
-				</button>
-			</div>
+				<div style={{ flex: '14%' }}>
+					<button className="rounded-2xl flex items-center bg-light-blue p-2 mx-auto" style={{ width: '50px', textAlign: 'center' }}>
+						<div style={{ fontSize: '14px' }} className="relative capitalize">Zoo Negara</div>
+					</button>
+				</div>
 
-			<div style={{ flex: '14%' }}>
-				<button className="rounded-2xl flex items-center bg-light-blue p-2 mx-auto" style={{ width: '50px', textAlign: 'center' }}>
-					<div style={{ fontSize: '14px' }} className="relative capitalize">National Museum</div>
-				</button>
+				<div style={{ flex: '14%' }}>
+					<button className="rounded-2xl flex items-center bg-light-blue p-2 mx-auto" style={{ width: '50px', textAlign: 'center' }}>
+						<div style={{ fontSize: '14px' }} className="relative capitalize">Batu Caves</div>
+					</button>
+				</div>
+
+				<div style={{ flex: '14%' }}>
+					<button className="rounded-2xl flex items-center bg-light-blue p-2 mx-auto" style={{ width: '50px', textAlign: 'center' }}>
+						<div style={{ fontSize: '14px' }} className="relative capitalize">National Museum</div>
+					</button>
+				</div>
 			</div>
 		</div>
 	); 
