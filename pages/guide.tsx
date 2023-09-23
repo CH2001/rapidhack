8d590dba1edbf5be09675a02d1a40a8f2db0ca09
Edmund_IdeaPad <elz.zhenming@gmail.com>
2023-09-23 13:52:48 +0800

    feat: create guide page

---
+++ pages/guide.tsx
@@ -0,0 +1,19 @@
+import BottomPageNavigation from './components/BottomNavBar';
+import TopPageNavigation from './components/TopNavBar';
+import Link from 'next/link';
+{/* <Link href="/contact">tesat</Link> */}
+
+const Guide = () => {
+
+  return (
+    <div className="relative">
+      <div className="[background:linear-gradient(180deg,_#4c2ffd_48.23%,_rgba(0,_224,_255,_0)_99.99%,_rgba(255,_67,_67,_0))]"> 
+        <TopPageNavigation/> 
+      </div>
+      This is guide.
+      <BottomPageNavigation/>
+    </div>
+  );
+};
+
+export default Guide;
