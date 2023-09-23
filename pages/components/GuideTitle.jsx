a3c91618ec8f39f4ad642b12164672670c7f6218
Edmund_IdeaPad <elz.zhenming@gmail.com>
2023-09-23 15:42:59 +0800

    chore: fix indentation

--- pages/components/GuideTitle.jsx
+++ pages/components/GuideTitle.jsx
@@ -6,24 +6,24 @@ import Link from "next/link";
 const GuideTitle = () => {
 	<div>
     <div style={{ flex: '14%' }}>
-				<button className="rounded-2xl flex items-center bg-light-blue p-2 mx-auto" style={{ width: '50px', textAlign: 'center' }}>
-						<div style={{ fontSize: '14px' }} className="relative capitalize">KL Twin Tower</div>
-				</button>
+			<button className="rounded-2xl flex items-center bg-light-blue p-2 mx-auto" style={{ width: '50px', textAlign: 'center' }}>
+				<div style={{ fontSize: '14px' }} className="relative capitalize">KL Twin Tower</div>
+			</button>
     </div>
 		<div style={{ flex: '14%' }}>
-				<button className="rounded-2xl flex items-center bg-light-blue p-2 mx-auto" style={{ width: '50px', textAlign: 'center' }}>
-						<div style={{ fontSize: '14px' }} className="relative capitalize">Zoo Negara</div>
-				</button>
+			<button className="rounded-2xl flex items-center bg-light-blue p-2 mx-auto" style={{ width: '50px', textAlign: 'center' }}>
+				<div style={{ fontSize: '14px' }} className="relative capitalize">Zoo Negara</div>
+			</button>
     </div>
 		<div style={{ flex: '14%' }}>
-				<button className="rounded-2xl flex items-center bg-light-blue p-2 mx-auto" style={{ width: '50px', textAlign: 'center' }}>
-						<div style={{ fontSize: '14px' }} className="relative capitalize">Batu Caves</div>
-				</button>
+			<button className="rounded-2xl flex items-center bg-light-blue p-2 mx-auto" style={{ width: '50px', textAlign: 'center' }}>
+				<div style={{ fontSize: '14px' }} className="relative capitalize">Batu Caves</div>
+			</button>
     </div>
 		<div style={{ flex: '14%' }}>
-				<button className="rounded-2xl flex items-center bg-light-blue p-2 mx-auto" style={{ width: '50px', textAlign: 'center' }}>
-						<div style={{ fontSize: '14px' }} className="relative capitalize">National Museum</div>
-				</button>
+			<button className="rounded-2xl flex items-center bg-light-blue p-2 mx-auto" style={{ width: '50px', textAlign: 'center' }}>
+				<div style={{ fontSize: '14px' }} className="relative capitalize">National Museum</div>
+			</button>
     </div>
 	</div>
 }
