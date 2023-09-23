9deb19b78897584aac4deb18bef86e4e84d8840e
CHUN HANSSEN <65500133+CH2001@users.noreply.github.com>
2023-09-23 09:30:23 +0000

    fix

--- pages/components/GuideTitle.jsx
+++ pages/components/GuideTitle.jsx
@@ -4,27 +4,32 @@ import Link from "next/link";
 
 
 const GuideTitle = () => {
-	<div>
-    <div style={{ flex: '14%' }}>
-			<button className="rounded-2xl flex items-center bg-light-blue p-2 mx-auto" style={{ width: '50px', textAlign: 'center' }}>
-				<div style={{ fontSize: '14px' }} className="relative capitalize">KL Twin Tower</div>
-			</button>
-    </div>
-		<div style={{ flex: '14%' }}>
-			<button className="rounded-2xl flex items-center bg-light-blue p-2 mx-auto" style={{ width: '50px', textAlign: 'center' }}>
-				<div style={{ fontSize: '14px' }} className="relative capitalize">Zoo Negara</div>
-			</button>
-    </div>
-		<div style={{ flex: '14%' }}>
-			<button className="rounded-2xl flex items-center bg-light-blue p-2 mx-auto" style={{ width: '50px', textAlign: 'center' }}>
-				<div style={{ fontSize: '14px' }} className="relative capitalize">Batu Caves</div>
-			</button>
-    </div>
-		<div style={{ flex: '14%' }}>
-			<button className="rounded-2xl flex items-center bg-light-blue p-2 mx-auto" style={{ width: '50px', textAlign: 'center' }}>
-				<div style={{ fontSize: '14px' }} className="relative capitalize">National Museum</div>
-			</button>
-    </div>
-	</div>
+	return (
+		<div>
+			<div style={{ flex: '14%' }}>
+				<button className="rounded-2xl flex items-center bg-light-blue p-2 mx-auto" style={{ width: '50px', textAlign: 'center' }}>
+					<div style={{ fontSize: '14px' }} className="relative capitalize">KL Twin Tower</div>
+				</button>
+			</div>
+
+			<div style={{ flex: '14%' }}>
+				<button className="rounded-2xl flex items-center bg-light-blue p-2 mx-auto" style={{ width: '50px', textAlign: 'center' }}>
+					<div style={{ fontSize: '14px' }} className="relative capitalize">Zoo Negara</div>
+				</button>
+			</div>
+
+			<div style={{ flex: '14%' }}>
+				<button className="rounded-2xl flex items-center bg-light-blue p-2 mx-auto" style={{ width: '50px', textAlign: 'center' }}>
+					<div style={{ fontSize: '14px' }} className="relative capitalize">Batu Caves</div>
+				</button>
+			</div>
+
+			<div style={{ flex: '14%' }}>
+				<button className="rounded-2xl flex items-center bg-light-blue p-2 mx-auto" style={{ width: '50px', textAlign: 'center' }}>
+					<div style={{ fontSize: '14px' }} className="relative capitalize">National Museum</div>
+				</button>
+			</div>
+		</div>
+	); 
 }
 export default GuideTitle;
